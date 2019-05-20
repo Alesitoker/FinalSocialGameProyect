6eb3a30c886550b7450d3ee54d59e3f45bd85b82
Alejandro <alesitonightray@gmail.com>
2019-05-20 08:18:55 +0200

    Initial commit

---
+++ app/src/main/java/com/iessaladillo/alejandro/finalsocialgameproyect/ui/login/LoginFragment.java
@@ -0,0 +1,6 @@
+package com.iessaladillo.alejandro.finalsocialgameproyect.ui.login;
+
+import android.support.v4.app.Fragment;
+
+public class LoginFragment extends Fragment {
+}
---
+++ app/src/main/java/com/iessaladillo/alejandro/finalsocialgameproyect/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iessaladillo.alejandro.finalsocialgameproyect.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iessaladillo.alejandro.finalsocialgameproyect.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
